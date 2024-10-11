106cf071a9b7d6ec9319b5ae476978f59b9dd320
Phuc Khang <khanglnpgcd191266@fpt.edu.vn>
2024-10-11 17:26:39 +0700

    create database for listing func

--- database/migrations/2024_10_11_091619_create_listings_table.php
+++ database/migrations/2024_10_11_091619_create_listings_table.php
@@ -13,6 +13,38 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->foreignId('location_id')->constrained('locations')->onDelete('cascade');
+            $table->foreignId('package_id')->nullable();
+
+            $table->string('image');
+            $table->string('thumbnail');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('phonenumber');
+            $table->string('email');
+            $table->string('address');
+            $table->string('seo_title');
+            $table->string('seo_description');
+            $table->string('file')->nullable();
+
+            $table->integer('views')->default(0);
+
+            $table->text('description');
+            $table->text('map_embed_code')->nullable();
+            $table->text('website')->nullable();
+            $table->text('fb_url')->nullable();
+            $table->text('x_url')->nullable();
+            $table->text('linked_url')->nullable();
+            $table->text('insta_url')->nullable();
+
+            $table->boolean('is_verified')->default(0);
+            $table->boolean('is_featured')->default(0);
+            $table->boolean('status')->default(1);
+
+            $table->date('expire_date');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
