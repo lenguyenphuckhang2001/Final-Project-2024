c96dc0ea6f9f2140f8c818a6f2cb07b6c33c01d3
Phuc Khang <khanglnpgcd191266@fpt.edu.vn>
2024-11-19 01:40:49 +0700

    Permission: Install spatie package for permission and setup files

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -196,9 +196,9 @@ namespace App\Models{
  *
  * @property int $id
  * @property string $phonenumber_one
- * @property string $phonenumber_two
+ * @property string|null $phonenumber_two
  * @property string $email_one
- * @property string $email_two
+ * @property string|null $email_two
  * @property string $address
  * @property string $map_embed_code
  * @property \Illuminate\Support\Carbon|null $created_at
@@ -784,9 +784,17 @@ namespace App\Models{
 /**
  * 
  *
+ * @property int $id
+ * @property string $content
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|TermAndConditions newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|TermAndConditions newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|TermAndConditions query()
+ * @method static \Illuminate\Database\Eloquent\Builder|TermAndConditions whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TermAndConditions whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TermAndConditions whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TermAndConditions whereUpdatedAt($value)
  */
 	class TermAndConditions extends \Eloquent {}
 }
@@ -817,12 +825,18 @@ namespace App\Models{
  * @property-read \App\Models\Membership|null $membership
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
  * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Permission> $permissions
+ * @property-read int|null $permissions_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Role> $roles
+ * @property-read int|null $roles_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
  * @property-read int|null $tokens_count
  * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User permission($permissions, $without = false)
  * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User role($roles, $guard = null, $without = false)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereAbout($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereAddress($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereAvatar($value)
@@ -842,6 +856,8 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUserType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereWebsite($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereXUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User withoutPermission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder|User withoutRole($roles, $guard = null)
  */
 	class User extends \Eloquent {}
 }
